4ec0827d97248aa55b8173b77fe725ee1050bbc2
aitap <krylov.r00t@gmail.com>
2017-12-28 00:26:30 +0300

    do not return stack garbage from callback; get the default random bit number from the library

--- gui.c
+++ gui.c
@@ -7,18 +7,22 @@ passwdqc_params_t params;
 Ihandle *password, *random_bits;
 
 int gen_passwd(Ihandle* button) {
-	(void)button; // I don't need it
+	(void)button; // only the text fields matter
 	params.qc.random_bits = IupGetInt(random_bits, "VALUE");
 	char *pass = passwdqc_random(&params.qc);
-	IupSetStrAttribute(password, "VALUE", pass);
+	IupSetStrAttribute(password, "VALUE", pass); // let IUP itself manage the string memory
 	free(pass);
+	return IUP_DEFAULT;
 }
 
 int main(int argc, char** argv) {
+	passwdqc_params_reset(&params);
 	IupOpen(&argc, &argv);
 
 	password = IupSetAttributes(IupText(NULL), "READONLY=YES,EXPAND=YES,VISIBLECOLUMNS=20,ALIGNNEMT=ACENTER");
-	random_bits = IupSetAttributes(IupText(NULL), "SPINMIN=24,SPINMAX=85,SPIN=YES,VALUE=47");
+	// magic numbers taken from the manual, unlikely to change ever, defined by the size of the word list
+	random_bits = IupSetAttributes(IupText(NULL), "SPINMIN=24,SPINMAX=85,SPIN=YES");
+	IupSetInt(random_bits, "VALUE", params.qc.random_bits);
 
 	IupShow(
 		IupDialog(
@@ -37,8 +41,6 @@ int main(int argc, char** argv) {
 			NULL)
 		)
 	);
-
-	passwdqc_params_reset(&params);
 
 	IupMainLoop();
 	IupClose();
