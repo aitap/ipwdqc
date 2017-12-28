bba5e06adbb2ef9e0916ca70844a493b87aafe1d
Ivan Krylov <krylov.r00t@gmail.com>
2017-12-28 13:03:18 +0300

    copy the password after generating it; do so on startup, too; also, focus the button

--- gui.c
+++ gui.c
@@ -6,12 +6,26 @@
 passwdqc_params_t params;
 Ihandle *password, *random_bits;
 
-int gen_passwd(Ihandle* button) {
-	(void)button; // only the text fields matter
+static void gen_passwd(void) {
 	params.qc.random_bits = IupGetInt(random_bits, "VALUE");
+
 	char *pass = passwdqc_random(&params.qc);
 	IupSetStrAttribute(password, "VALUE", pass); // let IUP itself manage the string memory
 	free(pass);
+
+	IupSetStrAttribute(password, "SELECTION", "ALL");
+	IupSetStrAttribute(password, "CLIPBOARD", "COPY");
+}
+
+static int button_cb(Ihandle* button) {
+	(void)button;
+	gen_passwd();
+	return IUP_DEFAULT;
+}
+
+static int spin_callback(Ihandle* spin, int value) {
+	(void)spin; (void)value;
+	gen_passwd();
 	return IUP_DEFAULT;
 }
 
@@ -19,28 +33,39 @@ int main(int argc, char** argv) {
 	passwdqc_params_reset(&params);
 	IupOpen(&argc, &argv);
 
-	password = IupSetAttributes(IupText(NULL), "READONLY=YES,EXPAND=YES,VISIBLECOLUMNS=20,ALIGNNEMT=ACENTER");
+	password = IupSetAttributes(IupText(NULL), "READONLY=YES,EXPAND=YES,VISIBLECOLUMNS=20,ALIGNMEMT=ACENTER");
+
 	// magic numbers taken from the manual, unlikely to change ever, defined by the size of the word list
 	random_bits = IupSetAttributes(IupText(NULL), "SPINMIN=24,SPINMAX=85,SPIN=YES");
 	IupSetInt(random_bits, "VALUE", params.qc.random_bits);
+	// IUP will cast the SPIN_CB callback back to correct
+	IupSetCallback(random_bits, "SPIN_CB", (Icallback)&spin_callback);
+
+	Ihandle *button = IupSetCallbacks(
+		IupButton("Generate", NULL),
+		"ACTION",
+		&button_cb,
+	NULL);
+	IupSetHandle("generate_button", button);
 
 	IupShow(
-		IupDialog(
-			IupVbox(
-				password,
-				IupHbox(
-					IupSetAttributes(IupLabel("Random bits:"),"PADDING=10x20"),
-					random_bits,
-					IupFill(),
-					IupSetCallbacks(
-						IupButton("Generate", NULL),
-						"ACTION",
-						&gen_passwd,
+		IupSetAttributes(
+			IupDialog(
+				IupVbox(
+					password,
+					IupHbox(
+						IupSetAttributes(IupLabel("Random bits:"),"ALIGNMENT=ACENTER:ACENTER"),
+						random_bits,
+						IupFill(),
+						button,
 					NULL),
-				NULL),
-			NULL)
+				NULL)
+			),
+			"TITLE=pwqgen,STARTFOCUS=generate_button"
 		)
 	);
+
+	gen_passwd();
 
 	IupMainLoop();
 	IupClose();
